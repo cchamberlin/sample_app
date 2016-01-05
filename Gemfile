be739dade5a0afbdd7304455469b96988a71cdab
cchamberlin <craig.chamberlin@gmail.com>
2016-01-05 03:54:17 +0000

    Finishing Chapter 6 and the User Model

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'rails', '4.2.4'
 gem 'sass-rails', '~> 5.0'
 # Use bootstrap-sass for CSS layout
 gem 'bootstrap-sass', '3.2.0.0'
+# Use bcrypt for passwords
+gem 'bcrypt', '3.1.7'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .coffee assets and views
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   before_save { self.email = email.downcase }
   
-  validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, uniqueness: { case_sensitive: false }, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  validates :password, length: { minimum: 6 }
+  validates :name, presence: true, length: { maximum: 50 }
 end
