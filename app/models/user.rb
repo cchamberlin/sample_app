1b127c6647a82ad0a4c0320f5eeae8ea8ac832a9
cchamberlin <craig.chamberlin@gmail.com>
2016-01-05 13:04:48 +0000

    Hotfix for bad email addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
   
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   
   before_save { self.email = email.downcase }
   
