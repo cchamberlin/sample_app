28a2f947fc36ec0318bfb68985f033170298b6f4
cchamberlin <craig.chamberlin@gmail.com>
2016-01-08 08:30:14 +0000

    Ending Chapter 8.1

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
-# TODO - Link to new on front page doesn't work.
 # TODO - Gravatar user isn't set up, see Chapter 7.1.4.
 
 
 Rails.application.routes.draw do
+
+  get 'sessions/new'
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
@@ -11,6 +12,9 @@ Rails.application.routes.draw do
   get 'about' => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
   
   resources :users
 
